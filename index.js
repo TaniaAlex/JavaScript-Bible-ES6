11ac88c6047fac9750077dd991433bf6ce04ee58
TaniaAlex <2tatyanan@gmail.com>
2019-09-28 12:50:42 +0200

    Added examples and comments

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+//***** ARRAY METHOD forEach()  */
+//***** forEach() method always RETURNS undefined */
 const myArray = [1, true, "abc"];
 
-// don't use
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
-}
+// // don't use
+// // for (let i = 0; i < myArray.length; i++) {
+// //   console.log(myArray[i]);
+// // }
 
+//Loop over an Array
 // use this instead
+
 myArray.forEach(element => console.log(element));
+console.log("=====");
+
+//***** EXAMPLE 2 */
+const fruits = ["Apple", "Banana", "Orange", "Lime", "Mango"];
+fruits.forEach(fruit => console.log(`Add ${fruit} to shopping list`));
+
+console.log("=====");
+//***** EXAMPLE 3 */
+const blackListContacts = ["Tim", "Tom", "Jerry", "Lucy", "Julia"];
+blackListContacts.forEach(blackListContact =>
+  console.log(`Never pick up when ${blackListContact} calls`)
+);
+console.log("=====");
+
+//***** EXAMPLE 4 */
+const apps = ["WhatsApp", "Instagram", "Facebook", "Telegram"];
+const appleStore = [];
+
+apps.forEach(app => appleStore.push(app));
+appleStore.forEach(app => console.log(`New update for ${app} is available`));
+console.log(appleStore);
