e49ae836e0926abd28a9d926a9bd0c1e6ba08861
TaniaAlex <2tatyanan@gmail.com>
2019-09-28 13:20:29 +0200

    Example 4 optimization

--- index.js
+++ index.js
@@ -13,22 +13,33 @@ const myArray = [1, true, "abc"];
 myArray.forEach(element => console.log(element));
 console.log("=====");
 
-//***** EXAMPLE 2 */
+// //***** EXAMPLE 2 */
 const fruits = ["Apple", "Banana", "Orange", "Lime", "Mango"];
 fruits.forEach(fruit => console.log(`Add ${fruit} to shopping list`));
 
 console.log("=====");
-//***** EXAMPLE 3 */
+// //***** EXAMPLE 3 */
 const blackListContacts = ["Tim", "Tom", "Jerry", "Lucy", "Julia"];
 blackListContacts.forEach(blackListContact =>
   console.log(`Never pick up when ${blackListContact} calls`)
 );
 console.log("=====");
 
-//***** EXAMPLE 4 */
+// //***** EXAMPLE 4 */
+// const apps = ["WhatsApp", "Instagram", "Facebook", "Telegram"];
+// const appleStore = [];
+
+// apps.forEach(app => appleStore.push(app));
+// appleStore.forEach(app => console.log(`New update for ${app} is available`));
+// // console.log(appleStore);
+console.log("=====");
+//***** EXAMPLE 4  optimized variant*/
 const apps = ["WhatsApp", "Instagram", "Facebook", "Telegram"];
 const appleStore = [];
+const updateInfo = app => {
+  return `New update for ${app} is available`;
+};
 
 apps.forEach(app => appleStore.push(app));
-appleStore.forEach(app => console.log(`New update for ${app} is available`));
-console.log(appleStore);
+appleStore.forEach(app => console.log(updateInfo(app)));
+// console.log(appleStore);
