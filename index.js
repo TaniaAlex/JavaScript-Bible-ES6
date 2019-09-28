ac04b02f73bd0e9db0b8fd53298b83070588afe6
TaniaAlex <2tatyanan@gmail.com>
2019-09-28 14:42:56 +0200

    Added comments

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
+//***** ARRAY METHOD map()  */
+//***** map() method RETURNS NEW ARRAY -> assign the result of map() method to new variable*/
+
 const myNumbers = [3, 5, 7, 2];
 
 // // for loop
-// let mySquareNumbers = [];
-
+// let mySquareNumbers1 = [];
 // for (let i = 0; i < myNumbers.length; i++) {
-//   mySquareNumbers.push(myNumbers[i] * myNumbers[i]);
+//   mySquareNumbers1.push(myNumbers[i] * myNumbers[i]);
 // }
+// console.log(mySquareNumbers1);
 
-// // map()
-// let mySquareNumbers = myNumbers.map(
-//   element => element * element
-// );
+//******************************* */
+// map()  -> initial Array remains unchanged, will produce NEW ARRAY*/
 
+let mySquareNumbers2 = myNumbers.map(element => element * element);
+console.log(mySquareNumbers2);
+
+//******************************* */
 // // map() with external function
-// const squareNumbers = element => element * element;
-// let mySquareNumbers = myNumbers.map(squareNumbers);
+const squareNumbers = element => element * element;
+let mySquareNumbers3 = myNumbers.map(squareNumbers);
+console.log(mySquareNumbers3);
 
+//******************************* */
 // // map() with external function with index
-// const squareNumbers = (element, index) => {
-//   console.log(
-//     "Element at the index " + index + " is " + element
-//   );
-//   return element * element;
-// };let mySquareNumbers = myNumbers.map(squareNumbers);
-//
+const squareNumbers4 = (element, index) => {
+  console.log(`At the index ${index} is element ${element}`);
+  return element * element;
+};
+let mySquareNumbers4 = myNumbers.map(squareNumbers4);
+console.log(mySquareNumbers4);
 
-// map() with Math.pow()
-let mySquareNumbers = myNumbers.map(element =>
-  Math.pow(element, 2)
-);
+//******************************* */
+// // map() with Math.pow()
+let mySquareNumbers5 = myNumbers.map(element => Math.pow(element, 2));
+console.log(mySquareNumbers5);
 
+// //******************************* */
 // // map() with direct call to Math.pow()
 // // "index" in Math.pow will be treated as exponent value
-// let mySquareNumbers = myNumbers.map(Math.pow);
-
-console.log(mySquareNumbers);
-console.log(myNumbers);
+// let mySquareNumbers6 = myNumbers.map(Math.pow);
+// console.log(mySquareNumbers6);
+// console.log(myNumbers);
