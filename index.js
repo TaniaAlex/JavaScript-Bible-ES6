2267bfae83c0ae9dac86664335dbf366b3e8f9ef
TaniaAlex <2tatyanan@gmail.com>
2019-09-29 17:24:34 +0200

    Challenge completed, one more example added

--- index.js
+++ index.js
@@ -7,7 +7,23 @@ const posts = [
 /* Create a function "findSinglePost" that will have two parameters - "postId" and "posts" and will return object with matched "postId".
 If post wasn't found - return "undefined"
 */
+const findSinglePost = (postId, posts) => {
+  return posts.find(post => post.postId === postId);
+};
 
 console.log(findSinglePost(6134, posts)); // { postId: 6134, commentsQuantity: 2 }
 
 console.log(findSinglePost(4511, posts)); // undefined
+
+// PRACTICE EXAMPLE
+const trees = [
+  { name: "bamboo", country: "India" },
+  { name: "maple", country: "Canada" },
+  { name: "oak", country: "Germany" },
+  { name: "magnolia", country: "USA" }
+];
+
+const findSingleTree = (name, trees) => {
+  return trees.find(tree => tree.name === name);
+};
+console.log(findSingleTree("oak", trees));
