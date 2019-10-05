34b7b4cce43a627075ef375711092a150890d8e8
TaniaAlex <2tatyanan@gmail.com>
2019-10-05 10:06:13 +0200

    Added another solution

--- index.js
+++ index.js
@@ -3,6 +3,21 @@
 Function is expected to return template literal. Sample function calls are below.
 */
 
+// // OPTION 1
+// const templateLiteral = num => {
+//   console.log(`Number is ${num}.`);
+//   console.log(`Square root of this number is ${Math.sqrt(num)}`);
+//   return num < 10
+//     ? `This number is less than 10`
+//     : `This number is greater than 10`;
+// };
+
+// OPTION 2 - teacher
+// implicitly return values of template literal
+const templateLiteral = num => `Number is ${num}.
+This number is ${num > 10 ? "greater" : "less"} than 10.
+Square root of this number is ${Math.sqrt(num)}.`;
+
 // TEST 1
 const myNumber = 9;
 console.log(templateLiteral(myNumber));
