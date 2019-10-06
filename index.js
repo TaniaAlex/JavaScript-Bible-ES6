07e16a2ed66621a2fd4ff033a834921872fa1bfe
TaniaAlex <2tatyanan@gmail.com>
2019-10-06 20:54:55 +0200

    Added solutions options

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ If second argument is absent, "weather" parameter should get value "Great weathe
 NOTE: Carefully compare your results with test calls results
 */
 
+// /******* Soltion WITHOUT default function parameter *******/
+// const weatherForecast = (city, weather) => {
+//   // option 1
+//   //   return weather === undefined
+//   //     ? `Weather forecast for ${city}: Great weather!`
+//   //     : `Weather forecast for ${city}: ${weather}`;
+//   // option 2
+//   weather = weather !== undefined ? weather : "Great weather!";
+//   return `Weather forecast for ${city}: ${weather}`;
+// };
+
+// /******* Soltion WITH default function parameter *******/
+const weatherForecast = (city, weather = "Great weather!") =>
+  `Weather forecast for ${city}: ${weather}`;
+
 console.log(weatherForecast("Dubai", "Sunny"));
 // Weather forecast for Dubai: Sunny
 
