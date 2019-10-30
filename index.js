8dafd34447388fec872885a286819156f720aac0
TaniaAlex <2tatyanan@gmail.com>
2019-10-30 11:02:04 +0100

    Solved, comments added

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 /*
 Modify parameters section in the "processQuantities" function to match console.log outputs
 */
-
-const processQuantities = (/* parameters */) => {
+/**
+ * use array destructuring directly in the parameter section and assign default value to the defaultQty parameter, if this element  is missing in the array, that was destructured
+ */
+const processQuantities = ([minQty, maxQty, defaultQty = 0]) => {
   console.log(minQty); // 8
   console.log(maxQty); // 29
   console.log(defaultQty); // 0
   return maxQty - minQty; // returns 21
 };
 
+// destructure this array directly in the parameter section of the function
 const qtyRange = [8, 29];
 
 processQuantities(qtyRange);
